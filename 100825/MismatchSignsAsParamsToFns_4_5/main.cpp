8e9f88c94cc8462e03f7af3a1c9fdba2ab3a8d34
tonyjustdevs <tonyjustdevs@gmail.com>
2025-08-10 16:07:01 +1000

    add 100825 MismatchSignsAsParamsToFns_4_5.cpp

---
+++ 100825/MismatchSignsAsParamsToFns_4_5/main.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+
+//void doSomething(unsigned int x){
+//    std::cout << "x is " << x << '\n';
+//}
+
+//void doSomething2(unsigned short x) {
+//    std::cout << "x is " << x << '\n';
+//}
+
+//void doSomething3(unsigned long x) {
+//    std::cout << "x is " << x << '\n';
+//}
+
+void doSomething4(unsigned char x) { //0-255 -> -1 -> 255?
+    //std::cout << "x is " << x << '\n';
+    std::cout << "x is " << static_cast<int>(x) << '\n';
+}
+
+void doSomething5(unsigned char x) { 
+    std::cout << "x is " << x << '\n'; //note above 127, ascii no exist
+}
+
+int main(){
+    //doSomething(-1);
+    //doSomething2(-1);
+    //doSomething3(-1);
+    doSomething4(-1); // 255 <- because char is 1 byte -> 256 values or
+    // [0-255], thus [-1] wraps to [255]
+    doSomething5(-191); // 165 -> A
+    return 0;
+}
