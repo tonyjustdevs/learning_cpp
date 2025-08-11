104cc56af948160ee4c88389f2b589c40c12543e
tony justdevs <tonyjustdevs@gmail.com>
2025-08-12 03:22:57 +1000

    add IEEE 754 compatible format comment in vs studio and commit via vs studio gui

--- 120825/FloatingPointNumbers_4_8/main.cpp
+++ 120825/FloatingPointNumbers_4_8/main.cpp
@@ -4,7 +4,7 @@
 int main() {
 	std::cout << std::left;
 	std::cout << std::setw(20) << "type:" << "true/false\n";
-
+	//IEEE 754 compatible format
 	std::cout << std::setw(20) << "float:"<<std::numeric_limits<float>::is_iec559 << "\n";
 	std::cout <<std::setw(20)<< "double:" << std::numeric_limits<double>::is_iec559 << "\n";
 	std::cout <<std::setw(20)<< "long double:" << std::numeric_limits<long double>::is_iec559<<"\n";
