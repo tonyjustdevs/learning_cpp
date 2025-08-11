17238b5361fa11927a89bb288fe5444faa1a9d80
tonyjustdevs <tonyjustdevs@gmail.com>
2025-08-11 21:15:06 +1000

    add 110825 significant notation solns to learn_ccp nb

---
+++ 110825/TrailingZeros_4_7/main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+int main() {
+	// Two Actual readings:
+	// - 87.0	grams
+	// - 87.000 grams
+			
+	// Assumption:
+	// - apple weight: [86.950,87.049] grams.
+
+	// Unknowns:
+	// - scale precise to the 0.1 gram
+	// - assistant rounded a bit.
+	return 0;
+}
