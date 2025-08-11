3b40f7be2b6ed2c046ceeb401c91bce2dc68ba1d
tonyjustdevs <tonyjustdevs@gmail.com>
2025-08-12 03:21:21 +1000

    add 120825 iec559 compliant checker FloatingPointNumbers_4_8.ipynb

---
+++ 120825/FloatingPointNumbers_4_8/main.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<limits>
+#include<iomanip>
+int main() {
+	std::cout << std::left;
+	std::cout << std::setw(20) << "type:" << "true/false\n";
+
+	std::cout << std::setw(20) << "float:"<<std::numeric_limits<float>::is_iec559 << "\n";
+	std::cout <<std::setw(20)<< "double:" << std::numeric_limits<double>::is_iec559 << "\n";
+	std::cout <<std::setw(20)<< "long double:" << std::numeric_limits<long double>::is_iec559<<"\n";
+	return 0;
+}
