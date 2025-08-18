e58bf2b1c9991fe5f0d9e605dd5ff65e0544e415
tonyjustdevs <tonyjustdevs@gmail.com>
2025-08-19 02:27:35 +1000

    add 190825 Std_Strings_5_7.sln

---
+++ 190825/Std_Strings_5_7/main.cpp
@@ -0,0 +1,14 @@
+﻿#include<iostream>
+#include<string>
+#include<cmath>
+#include<iomanip>
+
+int main() {
+	std::string name{ "cat" };
+	std::cout << "name: " << name << "\n";
+	std::cout << "sizeof(cat) : " << sizeof(name) << "\n";
+	name = "tiger";
+	std::cout << "name: " << name << "\n";
+	std::cout << "sizeof(tiger) : " << sizeof(name) << "\n";
+	return 0;
+}
