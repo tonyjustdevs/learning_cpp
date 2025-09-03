c9976b47d705422e79f67b7c8afb0652486a7faf
tonyjustdevs <tonyjustdevs@gmail.com>
2025-09-03 17:43:45 +1000

    add rl eval in OperatorPrecedence_6_1.soln

--- 030925/OperatorPrecedence_6_1/main.cpp
+++ 030925/OperatorPrecedence_6_1/main.cpp
@@ -1,6 +1,20 @@
 #include<iostream>
 
-int main() {
 
+int calcResult(int a, int b, int c) {// create fn with 3 args
+	int res{};
+	res = a + (b * c); //7=1+2*3 vs 5=3+2*1 
+	return res;
+};
+
+int getInput() { // create input takes 3 args (runs 3 times)
+	std::cout << "Add argument: ";
+	int input{};
+	std::cin >> input;
+	return input;
+}
+int main() { // note evaluation of arguments from L->R vs R->L
+	int res = calcResult(getInput(), getInput(), getInput());
+	std::cout << res; // entering 1,2,3 -> resulted in 5=3+2*1 ie R->L eval
 	return 0;
 }
