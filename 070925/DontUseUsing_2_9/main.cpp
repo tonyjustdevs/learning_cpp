437ec179a76f1dfc230ac22249077e29e4bfa0bd
tonyjustdevs <tonyjustdevs@gmail.com>
2025-09-07 18:36:47 +0700

    add 070925 IncludeHeadersAndDefines_2_10.sln

---
+++ 070925/DontUseUsing_2_9/main.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+using namespace std;
+
+int cout() {
+	return 420;
+}
+
+int main() {
+	cout << "ynot";
+	return 0;
+}
---
+++ 070925/IncludeHeadersAndDefines_2_10/main.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include "Header.h"
+//#define NAME "MESSI"
+int main() {
+	std::cout << "name: " << NAME;
+	return 0;
+}
+
+// 1. test #define variable
+// 2. use #defined variable
