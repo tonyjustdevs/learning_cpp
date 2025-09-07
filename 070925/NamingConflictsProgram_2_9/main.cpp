2c531f2c9a82edf52e70a9a38702282f42a0dafc
tonyjustdevs <tonyjustdevs@gmail.com>
2025-09-07 17:21:53 +0700

    add 070925 NamingConflictsProgram_2_9.sln

---
+++ 070925/NamingConflictsProgram_2_9/main.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+int add(int a, int b) {
+	return a + b;
+}
+int main() {
+// create same fn in two cpp fies
+
+// (expected) compilation: ok
+
+// (expected) linker: fail
+	return 0;
+}
