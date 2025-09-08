9e1afa19a076f5edacb7ea4381128e4e06613251
tonyjustdevs <tonyjustdevs@gmail.com>
2025-09-08 22:46:07 +0700

    add 080925 MultiCodeApp.sln

---
+++ 080925/MultiCodeApp/add.cpp
@@ -0,0 +1,7 @@
+//int add(int a, int b) {
+//	return a + b;
+//}
+#include"add.h"
+float add(int a, int b) {
+	return a + b;
+}
---
+++ 080925/MultiCodeApp/main.cpp
@@ -0,0 +1,7 @@
+#include"add.h"
+#include<iostream>
+
+int main() {
+	std::cout << "add(1,3):"<< add(1, 3);
+	return 0;
+}
