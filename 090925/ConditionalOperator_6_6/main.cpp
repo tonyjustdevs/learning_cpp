f7a5ade63aefec53ed2a7cf919f50f1c3ff04b86
tonyjustdevs <tonyjustdevs@gmail.com>
2025-09-10 01:01:54 +0700

    ConditionalOperator_6_6.sln

---
+++ 090925/ConditionalOperator_6_6/main.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+int main() {
+
+    int x{ 2 };
+    //std::cout << (x < 0) ? "negative" : "non-negative";
+    //std::cout << false ? "negative" : "non-negative";
+    // 0 is printed, std::cout is returned
+    //std::cout ? "negative" : "non-negative";
+    return 0;
+}
