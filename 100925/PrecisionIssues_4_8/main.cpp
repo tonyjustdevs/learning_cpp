606904b03bbed3ad4c6cd99c75960a54b9b087be
tonyjustdevs <tonyjustdevs@gmail.com>
2025-09-11 04:18:43 +0700

    add learn_cpp_08_sci_notation.ipynb

--- 100925/PrecisionIssues_4_8/main.cpp
+++ 100925/PrecisionIssues_4_8/main.cpp
@@ -2,12 +2,11 @@
 #include<iomanip>
 
 int main() {
-	// 1. create floating type variable with 10 sig figs (recall floats only sig to 7)
-	// 2. set precision to 9 via ionmanip  & std::set_precision(9) (ie output shown to set_prec)
-	// 3. see [rounding error] from cout
-
 	float a_cool_float{ 123456789.0f };
 	std::cout<<std::setprecision(9);
 	std::cout << "floating val:" << a_cool_float;
 	return 0;
 }
+	// 1. create floating type variable with 10 sig figs (recall floats only sig to 7)
+	// 2. set precision to 9 via ionmanip  & std::set_precision(9) (ie output shown to set_prec)
+	// 3. see [rounding error] from cout
